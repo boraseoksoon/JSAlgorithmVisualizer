9f069740f16fcc3e05892f930806a49fad00ae53
Seoksoon Jang <boraseoksoon@gmail.com>
2016-10-02 23:53:28 +0900

    ADD cpp single linked list skeleton code
    add cpp single linked list skeleton code.

---
+++ JSSAlgorithm/SingleLinkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  SingleLinkedList.cpp
+//  JSSAlgorithm
+//
+//  Created by Seoksoon Jang on 2016. 10. 2..
+//  Copyright © 2016년 Seoksoon Jang. All rights reserved.
+//
+
+#include "SingleLinkedList.hpp"
---
+++ JSSAlgorithm/SingleLinkedList.hpp
@@ -0,0 +1,14 @@
+//
+//  SingleLinkedList.hpp
+//  JSSAlgorithm
+//
+//  Created by Seoksoon Jang on 2016. 10. 2..
+//  Copyright © 2016년 Seoksoon Jang. All rights reserved.
+//
+
+#ifndef SingleLinkedList_hpp
+#define SingleLinkedList_hpp
+
+#include <stdio.h>
+
+#endif /* SingleLinkedList_hpp */
