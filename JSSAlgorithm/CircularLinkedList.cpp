132159a26576f2b7decf07ebeaed7ec856bd08ad
Seoksoon Jang <boraseoksoon@gmail.com>
2016-10-03 00:17:05 +0900

    ADD many series of linked list skeleton code in C/C++/Objective-C
    ADD many series of linked list skeleton code in C/C++/Objective-C.
    I love C family language.

---
+++ JSSAlgorithm/CircularLinkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  CircularLinkedList.cpp
+//  JSSAlgorithm
+//
+//  Created by Seoksoon Jang on 2016. 10. 3..
+//  Copyright © 2016년 Seoksoon Jang. All rights reserved.
+//
+
+#include "CircularLinkedList.hpp"
---
+++ JSSAlgorithm/DoubleCircularLinkedList.h
@@ -0,0 +1,13 @@
+//
+//  DoubleCircularLinkedList.h
+//  JSSAlgorithm
+//
+//  Created by Seoksoon Jang on 2016. 10. 3..
+//  Copyright © 2016년 Seoksoon Jang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DoubleCircularLinkedList : NSObject
+
+@end
