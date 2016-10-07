ebd2a5da3fca0dad03ad2675d84ce48e88b02683
Seoksoon Jang <boraseoksoon@gmail.com>
2016-10-07 18:31:37 +0900

    ADD new UI in master branch directly.
    after rebase, there is something not right working.
    UI update should be taken in UI branch you need to create after this.
    Since this is practice git project now, one time exception.

--- JSSAlgorithm/ViewController.mm
+++ JSSAlgorithm/ViewController.mm
@@ -15,11 +15,64 @@
 @end
 
 @implementation ViewController
-- (IBAction)enterDatastructureAction:(id)sender {
+
+const NSArray* datalist = @[@"Algorithm", @"DataStructure"];
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //
+    
+    if (indexPath.row == 0) {
+        NSLog(@"algorithm!");
+    } else if (indexPath.row == 1) {
+        NSLog(@"datastructure!");
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    /*
+     *   This is an important bit, it asks the table view if it has any available cells
+     *   already created which it is not using (if they are offScreen), so that it can
+     *   reuse them (saving the time of alloc/init/load from xib a new cell ).
+     *   The identifier is there to differentiate between different types of cells
+     *   (you can display different types of cells in the same table view)
+     */
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TopFrontMainTableView"];
+    
+    /*
+     *   If the cell is nil it means no cell was available for reuse and that we should
+     *   create a new one.
+     */
+    if (cell == nil) {
+        
+        /*
+         *   Actually create a new cell (with an identifier so that it can be dequeued).
+         */
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"TopFrontMainTableView"];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    
+    /*
+     *   Now that we have a cell we can configure it to display the data corresponding to
+     *   this row/section
+     */
+    cell.textLabel.text = [datalist objectAtIndex:indexPath.row];
+    
+    /* Now that the cell is configured we return it to the table view so that it can display it */
+    
+    return cell;
     
 }
-- (IBAction)enterAlgorithmAction:(id)sender {
-    
+
+/*
+ - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+ //
+ }
+ */
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [datalist count];
 }
 
 - (void)viewDidLoad {
@@ -37,42 +90,42 @@
     cout << list.popValue() << endl;
     
     /*
-    int i = 0;
-    int arrayCount = 0;
-    ArrayList* pList = NULL;
-    ArrayListNode* pValue = NULL;
-    
-    pList = createArrayList(6);
-    
-    if (pList != NULL){
-        ArrayListNode node;
-        
-        // List initialization : 1,3,5 add.
-        node.data = 1;
-        addALElement(pList, 0, node);
-        
-        node.data = 3;
-        addALElement(pList, 1, node);
-        
-        node.data = 5;
-        addALElement(pList, 2, node);
-        
-        displayArrayList(pList);
-        
-        // remove the first element
-        removeALElement(pList, 0);
-        displayArrayList(pList);
-        
-        arrayCount = getArrayListLength(pList);
-        
-        for (i=0; i<arrayCount; i++){
-            pValue = getALElement(pList, i);
-            printf("position[%d] - %d\n", i, pValue->data);
-        }
-        
-        deleteArrayList(pList);
-    }
-    */
+     int i = 0;
+     int arrayCount = 0;
+     ArrayList* pList = NULL;
+     ArrayListNode* pValue = NULL;
+     
+     pList = createArrayList(6);
+     
+     if (pList != NULL){
+     ArrayListNode node;
+     
+     // List initialization : 1,3,5 add.
+     node.data = 1;
+     addALElement(pList, 0, node);
+     
+     node.data = 3;
+     addALElement(pList, 1, node);
+     
+     node.data = 5;
+     addALElement(pList, 2, node);
+     
+     displayArrayList(pList);
+     
+     // remove the first element
+     removeALElement(pList, 0);
+     displayArrayList(pList);
+     
+     arrayCount = getArrayListLength(pList);
+     
+     for (i=0; i<arrayCount; i++){
+     pValue = getALElement(pList, i);
+     printf("position[%d] - %d\n", i, pValue->data);
+     }
+     
+     deleteArrayList(pList);
+     }
+     */
 }
 
 
